859fbe29c1f413950de67ebdc8881f8dbcc8cfd1
mariogmarq <mariogarciamarq@gmail.com>
2023-06-23 16:29:49 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="flexBlock",
+    version="0.0.1",
+    author="Mario Garcia Marquez",
+    keywords="blockchain FL federated-learning flexible",
+    packages=find_packages(),
+    install_requires=["flex"],
+)
