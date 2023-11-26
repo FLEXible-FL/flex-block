e921902993a3fde70f8bf9b71669b2a2959bf8c0
mariogmarq <mariogarciamarq@gmail.com>
2023-11-26 17:13:43 +0100

    New cifar-10 notebook example

---
+++ flexBlock/pool/primitives.py
@@ -0,0 +1,19 @@
+from flex.model import FlexModel
+from flexBlock.pool.decorators import send_weights_to_miner
+
+
+def collect_to_send_wrapper(func):
+    """Allows to change a function wrapped by `collect_client_weights` to a compatible flex-block function with `send_weights_to_miner`.
+
+    Args:
+        func: Function to be wrapped.
+
+    Returns:
+        Function that can be used with blockchain methods.
+    """
+
+    @send_weights_to_miner
+    def wrapped(model: FlexModel, *args, **kwargs):
+        return func.__wrapped__(model, args=args, kwargs=kwargs)
+
+    return wrapped
