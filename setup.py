136f285158f9c44109e019d31b2485a586511723
mariogmarq <mariogarciamarq@gmail.com>
2024-03-12 18:07:19 +0100

    Fix building process for the repo

--- setup.py
+++ setup.py
@@ -14,15 +14,35 @@ Copyright (C) 2024  Instituto Andaluz Interuniversitario en Ciencia de Datos e I
     You should have received a copy of the GNU Affero General Public License
     along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
+
 from setuptools import find_packages, setup
 
 setup(
     name="flexBlock",
     version="0.0.1",
     author="Mario Garcia Marquez",
+    author_email="mariogarciamarq@gmail.com",
     keywords="blockchain FL federated-learning flexible",
-    long_description=open("readme.md", "r", encoding="utf-8").read(),
+    long_description=open("README.md", "r", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
+    url="https://github.com/FLEXible-FL/flex-block",
     packages=find_packages(),
-    install_requires=["flex"],
+    install_requires=["flexible-fl"],
+    python_requires=">=3.8.10",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Science/Research",
+        "Topic :: Software Development :: Build Tools",
+        # Pick your license as you wish
+        "License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)",
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate you support Python 3. These classifiers are *not*
+        # checked by 'pip install'. See instead 'python_requires' below.
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3 :: Only",
+    ],
 )
